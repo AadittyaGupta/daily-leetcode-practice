1939d058618377d8bbb0c231ec84d793ca774608
AadittyaGupta <160506470+AadittyaGupta@users.noreply.github.com>
2025-06-22 08:34:46 +0530

    Add files via upload

---
+++ 0206-reverse-linked-list/reverse-linked-list.py
@@ -0,0 +1,41 @@
+from typing import Optional
+from typing import ListNode
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+
+        return prev    
+    
+
+
+
+
+
+# By recursive method
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        
+        new_head = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+
+        return new_head
+
+
+
